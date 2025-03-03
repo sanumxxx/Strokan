72d6e579d614278a51919fdb50c21051964b2521
sanumxxx <sanumxxx@yandex.ru>
2025-03-03 10:20:54 +0300

    v1.6

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,9 @@
 from flask import Flask
 from config import Config
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_folder='static',
+            template_folder='templates')
 app.config.from_object(Config)
 
 from app import routes
